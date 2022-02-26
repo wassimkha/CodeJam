34e623fa19458dd9eb328e0eacfee9e628586d09
wassimkha <43592500+wassimkha@users.noreply.github.com>
2022-02-25 21:49:40 -0500

    working MVP

--- AnswerQs/popup.js
+++ AnswerQs/popup.js
@@ -1,10 +1,6 @@
-function giveAnswer(a) {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", "Bearer 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");
-
-    var raw = JSON.stringify({
-        "prompt": "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: ",
+function giveAnswer(question) {
+    var data = JSON.stringify({
+        "prompt": "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: " + question,
         "temperature": 0,
         "max_tokens": 994,
         "top_p": 1,
@@ -15,21 +11,35 @@ function giveAnswer(a) {
         ]
     });
 
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
 
-    fetch("https://api.openai.com/v1/engines/text-davinci-001/completions", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+    xhr.addEventListener("readystatechange", function() {
+        if(this.readyState === 4) {
+            var res = this.responseText;
+            var obj = JSON.parse(res);
+            console.log(obj, obj.choices)
+            if (obj && obj.choices && obj.choices.length > 0) {
+                var completion = obj.choices[0].text;
+                if (completion.length > 0) {
+                    document.getElementById("output").innerHTML = completion;
+                } else {
+                    document.getElementById("output").innerHTML = "No answer found";
+                }
+            }
+        }
+    });
+
+    xhr.open("POST", "https://api.openai.com/v1/engines/text-davinci-001/completions");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Authorization", "Bearer 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");
+
+    xhr.send(data);
     return "hello there";
 }
 chrome.tabs.executeScript( {
     code: "window.getSelection().toString();"
 }, function(selection) {
-    document.getElementById("output").innerHTML = giveAnswer(selection[0]);
+    document.getElementById("output").innerHTML = "loading..."
+    giveAnswer(selection);
 });
